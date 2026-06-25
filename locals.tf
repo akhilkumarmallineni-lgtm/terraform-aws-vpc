bb57039b6ab85830706e4f46061b468323c72f6d
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-06-25 22:24:05 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 locals {
-  common_tags = {
-    project     = var.project_name
-    environment = var.environment
-    terraform   = "true"
-    common_name = local.common_name
-  }
-  common_name = "${var.project_name}-${var.environment}"
-  az_names = slice(data.aws_availability_zones.available.names, 0, 2) # here 2 is exlcusive
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+        Name = local.common_name
+    }
+    common_name = "${var.project}-${var.environment}" # roboshop-dev
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2) # here 2 is exlcusive
 }
